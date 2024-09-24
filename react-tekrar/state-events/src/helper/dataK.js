d296ae2d49ec0cbad1c95c5c6d0c165fb4c7a421
yelizesgi <yelizesgi2014@gmail.com>
2024-09-24 22:12:56 +0300

    added

---
+++ react-tekrar/state-events/src/helper/dataK.js
@@ -0,0 +1,58 @@
+export const dataK= [
+    {
+        id: 1,
+        categories: "dress",
+        color: "white",
+        img_url:"https://cdn.pixabay.com/photo/2021/04/16/07/22/ao-dai-6182834_640.jpg",
+        details:"Kadın elbise modelleri, moda dünyasında bir sembol ve ifade biçimi olarak varlığını sürdürüyor. Bu özel giyim parçası, sadece vücut hatlarını sarmakla kalmayıp aynı zamanda kişisel tarzı, özgüveni ve ruh halini yansıtan bir moda deklarasyonu haline geliyor. Kadın elbisesi, tarih boyunca değişen trendlere ve sosyal normlara ayak uydurarak evrim geçirmiş ve kadınların gardıroplarındaki yerini sağlamlaştırmıştır",
+    },
+    {
+        id: 2,
+        categories: "dress",
+        color: "white",
+        img_url:"https://cdn.pixabay.com/photo/2016/04/16/19/51/girl-1333640_640.jpg",
+        details:"Kadın elbise modelleri, moda dünyasında bir sembol ve ifade biçimi olarak varlığını sürdürüyor. Bu özel giyim parçası, sadece vücut hatlarını sarmakla kalmayıp aynı zamanda kişisel tarzı, özgüveni ve ruh halini yansıtan bir moda deklarasyonu haline geliyor. Kadın elbisesi, tarih boyunca değişen trendlere ve sosyal normlara ayak uydurarak evrim geçirmiş ve kadınların gardıroplarındaki yerini sağlamlaştırmıştır",
+    },
+    {
+        id: 3,
+        categories: "dress",
+        color: "white",
+        img_url:"https://cdn.pixabay.com/photo/2021/10/02/09/57/woman-6674780_640.jpg",
+        details:"Kadın elbise modelleri, moda dünyasında bir sembol ve ifade biçimi olarak varlığını sürdürüyor. Bu özel giyim parçası, sadece vücut hatlarını sarmakla kalmayıp aynı zamanda kişisel tarzı, özgüveni ve ruh halini yansıtan bir moda deklarasyonu haline geliyor. Kadın elbisesi, tarih boyunca değişen trendlere ve sosyal normlara ayak uydurarak evrim geçirmiş ve kadınların gardıroplarındaki yerini sağlamlaştırmıştır",
+    },
+    {
+        id: 4,
+        categories: "dress",
+        color: "white",
+        img_url:"https://cdn.pixabay.com/photo/2021/04/16/07/23/ao-dai-6182846_640.jpg",
+        details:"Kadın elbise modelleri, moda dünyasında bir sembol ve ifade biçimi olarak varlığını sürdürüyor. Bu özel giyim parçası, sadece vücut hatlarını sarmakla kalmayıp aynı zamanda kişisel tarzı, özgüveni ve ruh halini yansıtan bir moda deklarasyonu haline geliyor. Kadın elbisesi, tarih boyunca değişen trendlere ve sosyal normlara ayak uydurarak evrim geçirmiş ve kadınların gardıroplarındaki yerini sağlamlaştırmıştır",
+    },
+    {
+        id: 5,
+        categories: "dress",
+        color: "white",
+        img_url:"https://cdn.pixabay.com/photo/2021/12/07/02/38/woman-6851973_640.jpg",
+        details:"Kadın elbise modelleri, moda dünyasında bir sembol ve ifade biçimi olarak varlığını sürdürüyor. Bu özel giyim parçası, sadece vücut hatlarını sarmakla kalmayıp aynı zamanda kişisel tarzı, özgüveni ve ruh halini yansıtan bir moda deklarasyonu haline geliyor. Kadın elbisesi, tarih boyunca değişen trendlere ve sosyal normlara ayak uydurarak evrim geçirmiş ve kadınların gardıroplarındaki yerini sağlamlaştırmıştır",
+    },
+    {
+        id: 6,
+        categories: "dress",
+        color: "green",
+        img_url:"https://cdn.pixabay.com/photo/2023/08/17/05/55/ai-generated-8195533_640.png",
+        details:"Kadın elbise modelleri, moda dünyasında bir sembol ve ifade biçimi olarak varlığını sürdürüyor. Bu özel giyim parçası, sadece vücut hatlarını sarmakla kalmayıp aynı zamanda kişisel tarzı, özgüveni ve ruh halini yansıtan bir moda deklarasyonu haline geliyor. Kadın elbisesi, tarih boyunca değişen trendlere ve sosyal normlara ayak uydurarak evrim geçirmiş ve kadınların gardıroplarındaki yerini sağlamlaştırmıştır",
+    },
+    {
+        id: 7,
+        categories: "dress",
+        color: "light orange",
+        img_url:"https://cdn.pixabay.com/photo/2023/12/22/01/37/woman-8463055_640.jpg",
+        details:"Kadın elbise modelleri, moda dünyasında bir sembol ve ifade biçimi olarak varlığını sürdürüyor. Bu özel giyim parçası, sadece vücut hatlarını sarmakla kalmayıp aynı zamanda kişisel tarzı, özgüveni ve ruh halini yansıtan bir moda deklarasyonu haline geliyor. Kadın elbisesi, tarih boyunca değişen trendlere ve sosyal normlara ayak uydurarak evrim geçirmiş ve kadınların gardıroplarındaki yerini sağlamlaştırmıştır",
+    },{
+        id: 8,
+        categories: "dress",
+        color: "red",
+        img_url:"https://cdn.pixabay.com/photo/2016/10/31/02/29/woman-1784755_640.jpg",
+        details:"Kadın elbise modelleri, moda dünyasında bir sembol ve ifade biçimi olarak varlığını sürdürüyor. Bu özel giyim parçası, sadece vücut hatlarını sarmakla kalmayıp aynı zamanda kişisel tarzı, özgüveni ve ruh halini yansıtan bir moda deklarasyonu haline geliyor. Kadın elbisesi, tarih boyunca değişen trendlere ve sosyal normlara ayak uydurarak evrim geçirmiş ve kadınların gardıroplarındaki yerini sağlamlaştırmıştır",
+    }
+
+]
