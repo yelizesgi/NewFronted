2ba17315792b0a3318b82d24c5f168da4d095e6d
yelizesgi <yelizesgi2014@gmail.com>
2025-03-18 23:55:41 +0300

    data added

---
+++ react-tekrar/basic-project/src/helper/data.js
@@ -0,0 +1,32 @@
+export const image=[
+    {
+        id: 1,
+        categories: "Rose",
+        color: "blue",
+        img_url:"https://cdn.pixabay.com/photo/2013/07/21/13/00/rose-165819_640.jpg"
+    },
+    {
+        id: 2,
+        categories: "Rose",
+        color: "pink",
+        img_url:"https://cdn.pixabay.com/photo/2018/02/09/21/46/rose-3142529_640.jpg"
+    },
+    {
+        id: 3,
+        categories: "Rose",
+        color: "yellow",
+        img_url:"https://cdn.pixabay.com/photo/2023/10/01/16/01/rose-8287698_640.jpg"
+    },
+    {
+        id: 4,
+        categories: "Ortanca",
+        color: "Pink",
+        img_url:"https://cdn.pixabay.com/photo/2023/07/28/20/36/flower-8155951_640.jpg"
+    },
+    {
+        id: 1,
+        categories: "Rose",
+        color: "blue",
+        img_url:"https://cdn.pixabay.com/photo/2013/07/21/13/00/rose-165819_640.jpg"
+    },
+]
