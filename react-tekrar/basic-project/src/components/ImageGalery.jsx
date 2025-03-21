35b867a5c7c3a0afc4f42b5e70308301412bf033
yelizesgi <yelizesgi2014@gmail.com>
2025-03-21 16:59:39 +0300

    basic-projest style added

--- react-tekrar/basic-project/src/components/ImageGalery.jsx
+++ react-tekrar/basic-project/src/components/ImageGalery.jsx
@@ -2,14 +2,14 @@ import {data} from "../helper/data"
 
 const ImageGalery = ()=>{
     return(
-        <div className="container  text-center">
-          <h1 className="m-2 p-2 ">Flowers Image Galery</h1>
+        <div className="container  text-center border border-secondary-subtle ">
+          <h1 className="m-2 p-4 fst-italic bg-secondary-subtle rounded-3 ">Flowers Image Galery</h1>
           <div className="contain --bs-light-bg-subtle row  row-cols-2 ">
             {data.map(({id,categories, color, img_url}) =>(
                 <div key={id}>
                     <h1 className="header border-bottom border-danger text-bg-light">{categories}</h1>
-                    <img className="m-4 rounded-5 w-75 h-auto" src={img_url} alt={categories} />
-                    <h2>{color}</h2>
+                    <img className="m-4 rounded-5 w-75 h-50" src={img_url} alt={categories} />
+                    <h2 className="text-success bg-light rounded-3">{color}</h2>
                 </div>
             ))}
           </div>
