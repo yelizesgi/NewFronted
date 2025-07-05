1ac6825b5d967bc2d575db28abd4ed5096bc43cf
yelizesgi <yelizesgi2014@gmail.com>
2025-07-05 22:28:36 +0300

    vucud kitle index project added

---
+++ patika-frontendreactproject/project1-vucutkitleindex/src/components/Healtylife.jsx
@@ -0,0 +1,13 @@
+
+
+
+const Healtylife = () => {
+
+    return(
+        <>
+        <h1>Healty Life</h1>
+        </>
+    )
+}
+
+export default Healtylife
---
+++ patika-frontendreactproject/project1-vucutkitleindex/src/components/Home.jsx
@@ -0,0 +1,14 @@
+
+
+
+
+const Home = () =>{
+
+    return(
+        <>
+        <h1>Body Mass Index</h1>
+        </>
+    )
+}
+
+export default Home
