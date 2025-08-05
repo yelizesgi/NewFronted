8dbb81718d6e9b37932c612818d745f8ab5b78c5
yelizesgi <yelizesgi2014@gmail.com>
2025-08-05 23:01:46 +0300

    Conditionals ws01

--- javascript - tekrar/JS-Basics/conditionals.js
+++ javascript - tekrar/JS-Basics/conditionals.js
@@ -1,8 +1,30 @@
 
 //!-------Conditionals----------------//
 
-// 1. Bir sayının pozitif, negatif veya sıfır olduğunu bulan bir fonksiyon yazın.
-// 2. Bir yılın artık yıl olup olmadığını belirleyen bir kod yazın.
+//? 1. Bir sayının pozitif, negatif veya sıfır olduğunu bulan bir fonksiyon yazın.
+
+//* const sayi = +prompt('Lütfen bir sayı giriniz');
+//* if( sayi < 0 ){
+//*     console.log(`Girmiş olduğunuz sayi: ${sayi} Negatif`)
+//* }else if (sayi > 0){
+//*     console.log(`Girmiş olduğunuz sayi: ${sayi} Pozitif`)
+//* }else {
+//*     console.log(`Girmiş olduğunuz: ${sayi} dır.`)
+// };
+
+
+//? 2. Bir yılın artık yıl olup olmadığını belirleyen bir kod yazın.
+
+//* const artikyil = prompt('Lütfen bir yil giriniz.');
+
+//* if (artikyil % 4 === 0){
+//*     console.log(`Girmiş olduğunuz yil: ${artikyil} artik yildir`)
+//* }else if(artikyil % 100 === 0){
+//*    console.log(`Girmiş olduğunuz yil: ${artikyil} artik yildir.`)
+//* }else{
+//*     console.log(`Girmiş olduğunuz yil: ${artikyil} artik yil değildir`)
+//* }
+
 // 3. Girilen bir sayının tek mi çift mi olduğunu döndüren bir fonksiyon yazın.
 // 4. Bir kullanıcının yaşını alarak reşit olup olmadığını kontrol eden bir kod
 // yazın.
